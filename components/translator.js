a4a1038212f524dbcf9cc8f6dd8a055f3cf42c87
melsaqui <melanie.wayuaquino@gmail.com>
2024-10-23 18:20:59 +0800

    done most of functionality

--- components/translator.js
+++ components/translator.js
@@ -4,7 +4,103 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
+    AmericanBritish(str){
+        let strCopy = str.toLowerCase()
 
+       // console.log('her')
+       let same =true
+        let validTime = /^\d{1,2}:\d{2}$/
+
+        let words = str.split(' ')
+        let wordsCopy = strCopy.split(' ')
+        let newString=''; 
+        let bInjson = false;
+        //console.log(Object.keys(americanOnly)[0])
+        
+         for (let i=0;i<wordsCopy.length;i++){
+            if (Object.keys(americanOnly).includes(wordsCopy[i])){
+                newString = str.replace((words[i]),'<span class="highlight">'+americanOnly[wordsCopy[i]]+"</span>")
+                same=false
+            }
+            else if(Object.keys(americanToBritishSpelling).includes(wordsCopy[i])){
+                newString = str.replace((words[i]),'<span class="highlight">'+americanToBritishSpelling[wordsCopy[i]]+"</span>")
+                same=false
+            }
+            else if(Object.keys(americanToBritishTitles).includes(wordsCopy[i])){
+                let title = americanToBritishTitles[wordsCopy[i]];
+                if (words[i][0].toUpperCase()==words[i][0]){
+                    //start=this.getKeyByValue(americanToBritishTitles, wordsCopy[i])
+                    title = title.replace(title.charAt(0),title.charAt(0).toUpperCase())
+                }
+                newString = str.replace((words[i]),'<span class="highlight">'+ title +"</span>")
+                same=false
+            }
+            else if (words[i].match(validTime)){
+                newString = str.replace((words[i]),'<span class="highlight">'+ words[i].replace(":",'.')+"</span>")
+                same=false
+            }
+            //else
+            // newString+=words[i]+" "
+         }
+        if (same)
+            newString="Everything looks good to me!"
+         return newString;
+    
+    }
+    getKeyByValue(object, value) {
+        for (let prop in object) {
+            if (object.hasOwnProperty(prop)) {
+                if (object[prop] === value)
+                    return prop;
+            }
+        }
+    }
+    
+    BritishAmerican(str){
+        let strCopy = str.toLowerCase()
+
+        // console.log('her')
+        let same =true
+         let validTime = /^\d{1,2}.\d{2}$/
+ 
+         let words = str.split(' ')
+         let wordsCopy = strCopy.split(' ')
+ 
+         
+         let newString=''; 
+         
+          for (let i=0;i<words.length;i++){
+             if (Object.values(britishOnly).includes(wordsCopy[i])){
+                  newString = str.replace((words[i]),'<span class="highlight">'+ britishOnly[wordsCopy[i]] + "</span>")
+                 same= false
+                 
+             }
+             else if(Object.values(americanToBritishSpelling).includes(wordsCopy[i])){
+                 newString = str.replace((words[i]),'<span class="highlight">'+ this.getKeyByValue(americanToBritishSpelling, wordsCopy[i])+"</span>")
+                same =false
+             }
+             else if(Object.values(americanToBritishTitles).includes(wordsCopy[i])){
+                let title = this.getKeyByValue(americanToBritishTitles, wordsCopy[i]);
+                if (words[i][0].toUpperCase()==words[i][0]){
+                    //start=this.getKeyByValue(americanToBritishTitles, wordsCopy[i])
+                    title = title.replace(title.charAt(0),title.charAt(0).toUpperCase())
+                }
+                 newString = str.replace((words[i]),'<span class="highlight">'+ title +"</span>")
+                same=false
+             }
+             else if (words[i].match(validTime)){
+                 newString = str.replace((words[i]),'<span class="highlight">'+ words[i].replace(".",':')+"</span>")
+                same=false
+             }
+             //else 
+             //   newString+=words[i]+" "
+          }
+           if (same)
+            newString="Everything looks good to me!"
+          //console.log(str.toLowerCase() === newString.toLowerCase())
+          return newString;
+     
+     }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,32 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
+      let text = req.body.text;
+      let locale = req.body.locale;
+      console.log(text)
+      console.log(locale)
+      
+      if (locale ==''|| locale ==undefined || locale ==null || text==undefined || text==null)
+        return res.json({ error: 'Required field(s) missing' })
+      if(text==''|| text==undefined || text==null){
+        return res.json({ error: 'No text to translate' })
+      }
+      else if(locale !='american-to-british' && locale !='british-to-american' ){
+        return res.json({ error: 'Invalid value for locale field' })
+      }else{
+        if (locale ==='american-to-british'){
+
+          let translated=translator.AmericanBritish(text)
+          console.log(translated+"\n")
+          return (res.json({'text':text,"translation":translated}))
+        }
+        else{
+          let translated=translator.BritishAmerican(text)
+          console.log(translated+"\n")
+          return (res.json({'text':text,"translation":translated}))
+        }
+        
+      }
       
     });
 };
